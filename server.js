bf1030c05cc3022bcce602850b61331f66d37d4f
Juanco <juanco@Juans-Air>
2021-06-15 12:06:48 -0400

    fixing whitelist to deploy to heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connection.once('open', () => {
 })
 
 // Cors Middleware for Requests
-const whiteList = ['http://localhost:3000']
+const whiteList = ['http://localhost:3000', 'https://phonebook-frontend-project3.herokuapp.com/']
 const corsOptions = {
     origin: function (origin, callback) {
         if (whiteList.indexOf(origin) != -1) {
